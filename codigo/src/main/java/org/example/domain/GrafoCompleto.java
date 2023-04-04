455184634ece66c40e890e3f78bd5ea7a5e82959
Vitor Lany <101216359+vitorlany@users.noreply.github.com>
2023-04-04 09:05:15 -0300

    Grafo mutável + completo

---
+++ codigo/src/main/java/org/example/domain/GrafoCompleto.java
@@ -0,0 +1,10 @@
+package org.example.domain;
+
+public class GrafoCompleto extends Grafo {
+
+    public GrafoCompleto(String nome, int ordem) {
+        super(nome);
+        //TODO Auto-generated constructor stub
+    }
+
+}
---
+++ codigo/src/main/java/org/example/domain/GrafoMutavel.java
@@ -0,0 +1,35 @@
+package org.example.domain;
+
+public class GrafoMutavel extends Grafo {
+
+    public GrafoMutavel(String nome) {
+        super(nome);
+    }
+
+    public boolean addVertice(int id) {
+        return false;
+    }
+
+    public Vertice removeVertice(int id) {
+        return new Vertice(-1);
+    }
+
+    public boolean addAresta(int origem, int destino) {
+        return false;
+    }
+
+    public Aresta removeAresta(int id) {
+        return new Aresta(-1, -2);
+    }
+
+    public void carregar(String nomeArquivo) {
+
+    }
+
+
+    public void salvar(String nomeArquivo) {
+
+    }
+
+    
+}
