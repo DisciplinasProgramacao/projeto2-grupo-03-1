6c3e0b85436fa988783bcd8f9d115128ff5b4d23
Vitor Lany <101216359+vitorlany@users.noreply.github.com>
2023-04-04 09:32:10 -0300

    Grafo direcionado + não direcionado

--- codigo/src/main/java/org/example/domain/GrafoNaoDirecionado.java
+++ codigo/src/main/java/org/example/domain/GrafoNaoDirecionado.java
@@ -1,18 +1,20 @@
 package org.example.domain;
 
-public class GrafoDirecionado extends GrafoMutavel {
-
-    public GrafoDirecionado(String nome) {
+public class GrafoNaoDirecionado extends GrafoMutavel {
+    
+    public GrafoNaoDirecionado(String nome) {
         super(nome);
     }
 
     @Override
     public boolean addAresta(int origem, int destino, int peso) {
-        return super.addAresta(origem, destino, peso) ;
+        return super.addAresta(origem, destino, peso) 
+                && super.addAresta(destino, origem, peso);
     }
 
     @Override
     public Aresta removeAresta(int id, int destino) {
+        super.removeAresta(destino, id);
         return super.removeAresta(id, destino);
     }
 }
